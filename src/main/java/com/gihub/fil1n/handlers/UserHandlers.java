776947447aed5b553d77564e9767e4daea03aa48
fil1n <filatov.yaroslav@mail.com>
2019-07-14 02:50:05 +0600

    added test handler

--- src/main/java/com/gihub/fil1n/handlers/UserHandlers.java
+++ src/main/java/com/gihub/fil1n/handlers/UserHandlers.java
@@ -1,4 +1,29 @@
 package com.gihub.fil1n.handlers;
 
+import com.gihub.fil1n.HibernateInit;
+import com.gihub.fil1n.models.User;
+import io.javalin.Context;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.jetbrains.annotations.NotNull;
+
 public class UserHandlers {
+
+    public static void getUserById(@NotNull String id, @NotNull Context ctx) {
+        Session session;
+        Transaction transaction;
+
+        try {
+            session = HibernateInit.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            User user =  session.get(User.class, Long.valueOf(id));
+            transaction.commit();
+            session.close();
+
+            ctx.json(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
