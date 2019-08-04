7384912df349da4e7c3787e79aee08a812dab0b4
fil1n <filatov.yaroslav@mail.com>
2019-08-04 16:53:31 +0600

    updated main.java

--- src/main/java/com/gihub/fil1n/Main.java
+++ src/main/java/com/gihub/fil1n/Main.java
@@ -1,43 +1,36 @@
 package com.gihub.fil1n;
 
-import com.gihub.fil1n.handlers.UserHandlers;
-import com.gihub.fil1n.models.User;
+import com.gihub.fil1n.handlers.GroupCRUD;
+import com.gihub.fil1n.handlers.UserCRUD;
+import com.gihub.fil1n.jacksonClasses.JavalinJacksonUtils;
 import io.javalin.Javalin;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import io.javalin.json.JavalinJackson;
 
 public class Main {
-    public static void main(String[] args) {
-        User user = new User();
-
-        user.setFirstName("123");
-        user.setLastName("312");
-
-        Session session;
-        Transaction transaction;
+    public static void main(String[] args)  {
 
         try {
-
-            session = HibernateInit.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            session.save(user);
-
-            transaction.commit();
-            session.close();
-
+            HibernateInit.getSessionFactory();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        JavalinJackson.configure(JavalinJacksonUtils.getMapper());
 
         Javalin app = Javalin.create().port(9200).enableDebugLogging();
 
         app.routes(() ->
                 {
-                    app.get("/user/:id", ctx -> UserHandlers.getUserById(ctx.pathParam("id") , ctx));
+                    app.get("/user/:id", ctx -> UserCRUD.getUserById(ctx.pathParam("id") , ctx));
+                    app.post("/user", ctx -> UserCRUD.addUser(ctx));
+                    app.get("/group/:id", ctx -> GroupCRUD.getGroup(ctx.pathParam("id"), ctx));
+                    app.post("/group", ctx -> GroupCRUD.addGroup(ctx));
                 }
         );
 
         app.start();
     }
+
+    //TODO: add DAO, algo for group search, deploy
 }
