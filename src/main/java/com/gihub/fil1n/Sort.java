adf64aa85a230f7e1f70889121619c2a4459852c
fil1n <filatov.yaroslav@mail.com>
2019-08-19 16:33:33 +0600

    added

--- src/main/java/com/gihub/fil1n/Sort.java
+++ src/main/java/com/gihub/fil1n/Sort.java
@@ -1,4 +1,40 @@
 package com.gihub.fil1n;
 
+import com.gihub.fil1n.dao.GroupDao;
+import com.gihub.fil1n.models.City;
+import com.gihub.fil1n.models.Group;
+
+import java.util.ArrayList;
+
 public class Sort {
+
+    private static GroupDao dao = new GroupDao();
+
+    public static ArrayList<Group> getFilterGroups(Group exampleGroup) throws Exception {
+        try {
+
+            ArrayList<Group> filteredGroupList = new ArrayList<>();
+            ArrayList<Group> allGroups = (ArrayList) dao.getAllGroups();
+
+            for (int i = 0; i < allGroups.size(); ++i) {
+                if (allGroups.get(i).getCity().equals(exampleGroup.getCity()) && allGroups.get(i).isDateOfLivingProper(exampleGroup.getSettlementDate(), exampleGroup.getEjectionDate())) {
+                    filteredGroupList.add(allGroups.get(i));
+                }
+            }
+
+            return filteredGroupList;
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        throw  new Exception();
+    }
+
+    public static ArrayList<Group> makeSugarSort(Group exampleGroup, ArrayList<City> filtredList) {
+        return null;
+    }
+
+
+    private Sort() {}
 }
--- src/main/java/com/gihub/fil1n/Validator.java
+++ src/main/java/com/gihub/fil1n/Validator.java
@@ -1,4 +1,21 @@
 package com.gihub.fil1n;
 
+import org.apache.commons.validator.routines.EmailValidator;
+import org.jetbrains.annotations.NotNull;
+
 public class Validator {
+
+    private static EmailValidator validator = EmailValidator.getInstance();
+
+    public static boolean isEmailValid(@NotNull String email) {
+        return validator.isValid(email);
+    }
+
+//    public static boolean isPhoneNumberValid(@NotNull String number) {
+//
+//    }
+
+
+
+    private Validator() {}
 }
