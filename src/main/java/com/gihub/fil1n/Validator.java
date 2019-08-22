7993eaf52885ae5ec6a92e98d86ea630379b182b
fil1n <filatov.yaroslav@mail.com>
2019-08-22 21:50:58 +0600

    added test phone validator

--- src/main/java/com/gihub/fil1n/Validator.java
+++ src/main/java/com/gihub/fil1n/Validator.java
@@ -1,19 +1,31 @@
 package com.gihub.fil1n;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.jetbrains.annotations.NotNull;
 
 public class Validator {
 
     private static EmailValidator validator = EmailValidator.getInstance();
+    private static PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
 
     public static boolean isEmailValid(@NotNull String email) {
         return validator.isValid(email);
     }
 
-//    public static boolean isPhoneNumberValid(@NotNull String number) {
-//
-//    }
+    public static boolean isPhoneNumberValid(@NotNull String number, @NotNull String code) throws Exception {
+
+        try {
+            Phonenumber.PhoneNumber swissNumberProto = phoneUtil.parse(number, code);
+            return phoneUtil.isValidNumber(swissNumberProto);
+        } catch (NumberParseException e) {
+            e.printStackTrace();
+        }
+
+        throw new Exception();
+    }
 
 
 
