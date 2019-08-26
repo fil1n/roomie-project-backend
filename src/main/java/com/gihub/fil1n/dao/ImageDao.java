3b5bb847ea553243ee5851f091f71511982c3586
fil1n <filatov.yaroslav@mail.com>
2019-08-26 23:12:10 +0600

    added

--- src/main/java/com/gihub/fil1n/dao/ImageDao.java
+++ src/main/java/com/gihub/fil1n/dao/ImageDao.java
@@ -1,4 +1,41 @@
 package com.gihub.fil1n.dao;
 
+import com.gihub.fil1n.HibernateInit;
+import com.gihub.fil1n.models.Image;
+import com.sun.istack.NotNull;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class ImageDao {
+
+    public void saveImage(@NotNull Image image) {
+        Transaction transaction;
+        Session session;
+
+        try {
+            session = HibernateInit.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(image);
+            transaction.commit();
+            session.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteImage(@NotNull Image image) {
+        Transaction transaction;
+        Session session;
+
+        try {
+            session = HibernateInit.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(image);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
